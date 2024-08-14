a48eeebf6bf8f181d0e75dcc53fdda343c535491
GwonDooHyeon <secure3141@gmail.com>
2024-08-15 02:11:46 +0900

    쓰레드 로컬 - 동시성 문제(예제 코드)

---
+++ src/test/java/spring/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,39 @@
+package spring.advanced.trace.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.threadlocal.code.FieldService;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private final FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> fieldService.logic("userA");
+        Runnable userB = () -> fieldService.logic("userB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 발생 x
+        sleep(100); // 동시성 문제 발생 o
+        threadB.start();
+
+        sleep(3000);
+        log.info("main end");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
